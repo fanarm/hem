a64feb5ec8b19b8919fe329e54f5a6e7c09811ac
Xun Jiang <xun.jiang@live.com>
2015-12-31 01:42:01 +0800

    Add history page to view previous samples. Add no cache response to browser request.

--- site/hemServer.py
+++ site/hemServer.py
@@ -7,7 +7,7 @@ import datetime
 import posix_ipc as ipc
 import os
 
-REGULAR_LOG_FILE = "/home/pi/hem/log/pmth.log"
+LOG_FILE_PATH = "/home/pi/hem/log/"
 REGULAR_LOG_REGEX = "(\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}) PM2\.5:(\d+) Temp:(\d+) RH:(\d+)%"
 SHM_NAME_HEM_PRESENT_DATA = "/hem_data"
 SHM_SIZE_HEM_PRESENT_DATA = 256
@@ -34,15 +34,20 @@ app = Flask(__name__)
 @app.route("/") 
 def showStatus():
    recline = shared_memory_readline(SHM_NAME_HEM_PRESENT_DATA, SHM_SIZE_HEM_PRESENT_DATA)
-   ## reglogfile = open(REGULAR_LOG_FILE,'r')
-   ## recline = reglogfile.readline()
-   ## reglogfile.close()
    rec_mo = re.search(REGULAR_LOG_REGEX, recline)
    timestamp = rec_mo.group(1)
    pmvalue = rec_mo.group(2)
    tvalue = rec_mo.group(3)
    rhvalue = rec_mo.group(4)
    return render_template('hem_template.html', ts=timestamp, pm=pmvalue, t=tvalue, rh=rhvalue)
+
+@app.route("/history.html")
+def serveHistory():
+   curtime = datetime.datetime.now()
+   filename = curtime.strftime("pmth_plot_%Y_%m_%d.log")
+   logfile = open(LOG_FILE_PATH+filename,'r')
+   lines = logfile.readlines()
+   return render_template('history_template.html', header=lines[0], entries=lines[1:])
 
 @app.route("/chart/<path:filename>")
 def serveChart(filename):
@@ -53,6 +58,12 @@ def serveNowChart(filename):
    curtime = datetime.datetime.now()
    timestr = curtime.strftime("_%Y_%m_%d.png")
    return send_from_directory("/home/pi/hem/pic", filename+timestr)
+
+@app.after_request
+def add_header(response):
+   response.headers['Cache-Control']='no-cache,no-store,must-revalidate'
+   response.headers['Pragma']='no-cache'
+   return response
 
 if __name__ == "__main__" :
    app.run('0.0.0.0')
